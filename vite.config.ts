0c7a931419a6eccfa842f4fd9ef7110e282e9854
Stefan <stefangraf2@gmx.de>
2026-01-06 14:02:56 +0000

    quickfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'SQL-for-Files'
+  base: "SQL-for-Files",
   plugins: [react()],
-})
+});
